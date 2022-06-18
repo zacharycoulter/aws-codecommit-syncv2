e6dd44638f749b9d7cf0674cdba0c32b6f5dd7f5
hanukoon <hanu@hanukoon.com>
2022-06-19 01:23:45 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git remote add sync ${CodeCommitUrl}
-git push sync --mirror
+git push sync ${branch} -f
