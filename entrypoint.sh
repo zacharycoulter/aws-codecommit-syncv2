e0703c091b63f991050bf8c254d0884f59911b91
hectorcoy <112994905+hectorcoy@users.noreply.github.com>
2023-05-18 19:28:13 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,6 @@ CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${Repo
 git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
+git pull origin ${Branch}
 git remote add sync ${CodeCommitUrl}
 git push sync ${Branch} -f
